425534f2e85fda689c6ddc47fa0147cd1a9afbee
samuelli1903 <anrohakiselev117@gmail.com>
2021-10-28 19:43:33 +0300

    FIx find surname

--- Program.cs
+++ Program.cs
@@ -20,77 +20,113 @@ namespace PatientFind
             //PatientList.Add(p5);
             Console.WriteLine("Enter criteria of finding");
             string criteria = Console.ReadLine();
+            Console.WriteLine("Commands for input: ");
+            Console.WriteLine("To search by name: Findbyname");
+            Console.WriteLine("To search by surname: Findbysurname");
+            Console.WriteLine("To search by status: Findcritical");
+            Console.WriteLine("To search by status: Findlessthan");
+            Console.WriteLine("To exit the program enter: Close program");
+            Console.Write("Enter criteria of finding:");
             criteria = criteria.ToLower();
             criteria = criteria.Trim();
-            
-            switch (criteria)
+            bool factor = true;
+            while (factor == true)
             {
-                
-                case "findbyname":
-                    {
-                        Console.WriteLine("Enter Name:");
-                        string name = Console.ReadLine();
-                        if(FindName(PatientList, name, out Patient p))
-                        {
-                            Console.WriteLine(p.Name + " " + p.SurName);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Patient not found");
-                        }
-                        break;
-                    }
-                case "findcritical":
-
+                switch (criteria)
                 {
-                        if (FindCritical(PatientList, out List<Patient> p))
+                    case "findbysurname":
                         {
-                            foreach(var c in p)
-                                
-                            Console.WriteLine(c.Name + " " + c.SurName);
+                            Console.WriteLine("Enter Surname:");
+                            string surname = Console.ReadLine();
+                            surname = surname.ToLower();
+                            surname = surname.Trim();
+                            if (FindBySurName(PatientList, surname, out Patient p))
+                            {
+                                Console.WriteLine(p.Name + " " + p.SurName);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Patient not found");
+                            }
+                            break;
                         }
-                        else
+
+                    case "findbyname":
                         {
-                            Console.WriteLine("Patient not found");
+                            Console.WriteLine("Enter Name:");
+                            string name = Console.ReadLine();
+                            name = name.ToLower();
+                            name = name.Trim();
+                            if (FindByName(PatientList, name, out Patient p))
+                            {
+                                Console.WriteLine(p.Name + " " + p.SurName);
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("Patient not found");
+                            }
+                            break;
                         }
-                        break;
+                    case "findcritical":
+
+                        {
+                            if (FindCritical(PatientList, out List<Patient> p))
+                            {
+                                foreach (var c in p)
+                                Console.WriteLine(c.Name + " " + c.SurName);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Patient not found");
+                            }
+                            break;
+                        }
+                    case "findlessthan":
+                        {
+                            Console.WriteLine("Enter age:");
+                            int age = int.Parse(Console.ReadLine());
+
+                            if (LessThan(PatientList, age, out List<Patient> p))
+                            {
+                                foreach (var a in p)
+                                    Console.WriteLine(a.Name + " " + a.SurName);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Patient not found");
+                            }
+                            break;
+                        }
+                    case "close program":
+                        {
+                            factor = false;
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Unknown command. Try again");
+                            break;
+                        }
+
+
                 }
-                case "findlesstham":
-                    {
-                        Console.WriteLine("Enter age:");
-                        int age = int.Parse(Console.ReadLine());
-
-                        if (LessTham(PatientList,age, out List<Patient> p))
-
-                        {                           
-                               foreach (var a in p)
-                               Console.WriteLine(a.Name + " " + a.SurName);
-                            
-                        }
-                        else
-                        {
-                            Console.WriteLine("Patient not found");
-                        }
-                        break;
-                    }
-                default:
-                    {
-                        Console.WriteLine("Unknown command");
-                        break;
-                    }
+                Console.WriteLine("Enter command");
+                criteria = Console.ReadLine();
 
             }
 
+
         }
-        static bool FindCritical(List<Patient>patients, out List<Patient> Found)
+        static bool FindCritical(List<Patient> patients, out List<Patient> Found)
         {
             Found = new();
             foreach (var p in patients)
             {
-                if ("critical"==p.Status)
+                if ("critical" == p.Status)
                 {
                     Found.Add(p);
-                    
+
                 }
 
             }
@@ -98,14 +134,14 @@ namespace PatientFind
                 return true;
             else
                 return false;
-            
+
         }
-        static bool LessTham(List<Patient> patients, int age, out List<Patient> Found)
+        static bool LessThan(List<Patient> patients, int age, out List<Patient> Found)
         {
             Found = new();
             foreach (var p in patients)
             {
-                if (age>=p.age)
+                if (age >= p.age)
                 {
                     Found.Add(p);
 
@@ -118,16 +154,29 @@ namespace PatientFind
                 return false;
 
         }
-        static bool FindName(List<Patient>patients,string name, out Patient Found)
+        static bool FindBySurName(List<Patient> patients, string surname, out Patient Found)
         {
-            foreach(var p in patients)
+            foreach (var p in patients)
             {
-                if(name==p.Name)
+                if (surname == p.SurName.ToLower())
                 {
                     Found = p;
                     return true;
                 }
-                    
+            }
+            Found = null;
+            return false;
+        }
+        static bool FindByName(List<Patient> patients, string name, out Patient Found)
+        {
+            foreach (var p in patients)
+            {
+                if (name == p.Name.ToLower())
+                {
+                    Found = p;
+                    return true;
+                }
+
             }
             Found = null;
             return false;
